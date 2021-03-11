d6e2625d43977b8ccf045d938f8bd2e52113ee3a
facundo <facuraposo@gmail.com>
2021-03-11 15:37:50 -0300

    Nav bar

---
+++ src/components/nav-bar/nav-bar.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export const NavBar = () => {
+    return(
+        <div>
+            <h3>Red Sport</h3>
+            <ul>
+                <li><a href="#">Calzado</a></li>
+                <li><a href="#">Vestimenta</a></li>
+                <li><a href="#">Accesorios</a></li>
+                <li><a href="#">Fitness</a></li>
+            </ul>
+        </div>
+        );
+};
