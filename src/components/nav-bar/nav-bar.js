1191d1bd7a3452f61f1597ff83629b6328b4d9b8
facundo <facuraposo@gmail.com>
2021-03-13 17:57:25 -0300

    navbar fixed & cartwidget

--- src/components/nav-bar/nav-bar.js
+++ src/components/nav-bar/nav-bar.js
@@ -1,15 +1,24 @@
 import React from "react";
+import {NavLink, withRouter} from "react-router-dom";
+import CartWidget from "../cart-widget/CartWidget";
 
 export const NavBar = () => {
     return(
-        <div>
-            <h3>Red Sport</h3>
-            <ul>
-                <li><a href="#">Calzado</a></li>
-                <li><a href="#">Vestimenta</a></li>
-                <li><a href="#">Accesorios</a></li>
-                <li><a href="#">Fitness</a></li>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container-fluid">
+            <a className="navbar-brand" href="#">Red Sport</a>
+            <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+                <li className="nav-item active"><a className="nav-link" href="#">Home<span className="sr-only"></span></a></li>
+                <li className="nav-item"><a className="nav-link" href="#">Vestimenta</a></li>
+                <li className="nav-item"><a className="nav-link" href="#">Accesorios</a></li>
+                <li className="nav-item"><a className="nav-link" href="#">Calzado</a></li>
             </ul>
+            
+            </div>
+            <CartWidget />
         </div>
+        </nav>
+
         );
 };
